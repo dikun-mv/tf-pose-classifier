da7780defd047fe280ce956c63270b26c8d5589a
Maxim Dikun <dikun.mv@gmail.com>
2018-10-08 13:13:45 +0300

    Add RingBuffer class

--- src/main.py
+++ src/main.py
@@ -1,35 +1,43 @@
 import tensorflow as tf
+import numpy as np
+import cv2 as cv
 
 from os import path
-from time import time
 from tf_pose import Estimator
-from ws_publisher import Publisher, poses_to_dto
 from cam_reader import Reader
 
 TF_CONFIG = tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.2))
 MODEL_PATH = path.realpath(path.join(path.dirname(__file__), '../models/mobilenet.pb'))
 
+
+class RingBuffer:
+    def __init__(self, size):
+        self.head = 0
+        self.size = size
+        self.data = [None for _ in range(size)]
+
+    def save(self, item):
+        if self.head == self.size:
+            self.head = 0
+
+        self.data[self.head] = item
+        self.head += 1
+
+    def dump(self):
+        return self.data
+
+
 if __name__ == '__main__':
     reader = Reader(1920, 1080)
-    publisher = Publisher('0.0.0.0', 9090)
-    estimator = Estimator(MODEL_PATH, (656, 368), TF_CONFIG)
+    estimator = Estimator(MODEL_PATH, (368, 368), TF_CONFIG)
+    buffer = RingBuffer(100)
 
-    start_time = time()
     frame_number = 0
 
     while True:
-        frame_number += 1
         frame = reader.read()
+        frame_number += 1
 
         if frame is None: break
 
         poses = estimator.inference(frame, upsample_size=4.0)
-        publisher.send(poses_to_dto(poses))
-
-        elapsed_time = time() - start_time
-
-        if elapsed_time >= 1:
-            print('FPS: {}'.format(int(frame_number / elapsed_time)))
-
-            start_time = time()
-            frame_number = 0
